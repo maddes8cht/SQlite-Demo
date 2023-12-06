d85c725f61355a9b012baef9ffba2691af83c50b
maddes8cht <mathias.bachmann@gmx.net>
2023-12-06 20:15:51 +0100

    Move functions to sqlite-helper.cpp

--- src/sqlite-demo.cpp
+++ src/sqlite-demo.cpp
@@ -1,97 +1,4 @@
-/**
- * @file
- * @brief Example program demonstrating SQLite functionality.
- *
- * This program illustrates basic SQLite operations, including:
- * - Opening a database
- * - Creating a table
- * - Inserting data into the table
- * - Executing a SELECT query and printing the results
- *
- * @author Mathias Bachmann
- * @date 2023/12/05
- */
-
-#include <iostream>
-#include <stdexcept>
-#include <sqlite3.h>
-
-/**
- * @brief Custom exception class for SQLite-related errors.
- *
- * This exception class is derived from std::runtime_error and is used to handle errors
- * that occur during SQLite operations.
- */
-class SQLiteException : public std::runtime_error {
-public:
-    /**
-     * @brief Constructs an SQLiteException with a given error message.
-     *
-     * @param[in] message The error message.
-     */
-    SQLiteException(const std::string &message) : std::runtime_error(message) {}
-};
-
-/**
- * @brief SQLite Database Connection
- *
- * This function opens a SQLite database connection.
- *
- * @param[out] db SQLite database connection
- * @param[in] dbName Name of the database
- * @throw SQLiteException on failure
- */
-void openDatabase(sqlite3 *&db, const char *dbName) {
-    int rc = sqlite3_open(dbName, &db);
-    if (rc != SQLITE_OK) {
-        throw SQLiteException("Error opening the database: " + std::string(sqlite3_errmsg(db)));
-    }
-}
-
-/**
- * @brief Execute SQL Statement
- *
- * This function executes a given SQL statement.
- *
- * @param[in] db SQLite database connection
- * @param[in] sqlStatement SQL statement to be executed
- * @throw SQLiteException on failure
- */
-void executeSqlStatement(sqlite3 *db, const char *sqlStatement) {
-    int rc = sqlite3_exec(db, sqlStatement, 0, 0, 0);
-    if (rc != SQLITE_OK) {
-        sqlite3_close(db);
-        throw SQLiteException("Error executing SQL statement: " + std::string(sqlite3_errmsg(db)));
-    }
-
-    // No need to close the database connection here; it's already done in the functions
-}
-
-/**
- * @brief Execute SELECT Statement and Print Results
- *
- * This function executes a SELECT SQL statement and prints the results.
- *
- * @param[in] db SQLite database connection
- * @param[in] selectStatement SELECT SQL statement to be executed
- * @throw SQLiteException on failure
- */
-void executeAndPrintSelectStatement(sqlite3 *db, const char *selectStatement) {
-    int rc = sqlite3_exec(db, selectStatement, [](void *data, int argc, char **argv, char **colName) -> int {
-        for (int i = 0; i < argc; i++) {
-            std::cout << colName[i] << ": " << (argv[i] ? argv[i] : "NULL") << " | ";
-        }
-        std::cout << std::endl;
-        return 0;
-    }, 0, 0);
-
-    if (rc != SQLITE_OK) {
-        sqlite3_close(db);
-        throw SQLiteException("Error executing SELECT statement: " + std::string(sqlite3_errmsg(db)));
-    }
-
-    // No need to close the database connection here; it's already done in the functions
-}
+#include "sqlite-helper.h"
 
 /**
  * @brief Main function for the SQLite demonstration program.
@@ -125,7 +32,6 @@ int main() {
 
         // Close the database
         sqlite3_close(db);
-
 
     } catch (const SQLiteException &ex) {
         std::cerr << "SQLite Exception: " << ex.what() << std::endl;
