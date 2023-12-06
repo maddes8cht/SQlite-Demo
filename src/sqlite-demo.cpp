e51c34fe57d228c7486f0998da7a757ba24ef7d0
maddes8cht <mathias.bachmann@gmx.net>
2023-12-06 01:02:19 +0100

    SQlite-Demo: Refactor into functions, use exception error handling, create doxygen Comments

--- src/sqlite-demo.cpp
+++ src/sqlite-demo.cpp
@@ -1,53 +1,83 @@
-#include <iostream>
-#include <sqlite3.h>
-
 /**
- * @brief Main function to demonstrate SQLite functionality.
+ * @file
+ * @brief Example program demonstrating SQLite functionality.
  *
- * This program demonstrates basic SQLite operations, including:
+ * This program illustrates basic SQLite operations, including:
  * - Opening a database
  * - Creating a table
  * - Inserting data into the table
  * - Executing a SELECT query and printing the results
  *
- * @return 0 on successful execution, non-zero on error.
+ * @author Mathias Bachmann
+ * @date 2023/12/05
  */
-int main() {
-    // SQLite Database Connection
-    sqlite3 *db;
-    int rc = sqlite3_open("example.db", &db);
 
+#include <iostream>
+#include <stdexcept>
+#include <sqlite3.h>
+
+/**
+ * @brief Custom exception class for SQLite-related errors.
+ *
+ * This exception class is derived from std::runtime_error and is used to handle errors
+ * that occur during SQLite operations.
+ */
+class SQLiteException : public std::runtime_error {
+public:
+    /**
+     * @brief Constructs an SQLiteException with a given error message.
+     *
+     * @param[in] message The error message.
+     */
+    SQLiteException(const std::string &message) : std::runtime_error(message) {}
+};
+
+/**
+ * @brief SQLite Database Connection
+ *
+ * This function opens a SQLite database connection.
+ *
+ * @param[out] db SQLite database connection
+ * @param[in] dbName Name of the database
+ * @throw SQLiteException on failure
+ */
+void openDatabase(sqlite3 *&db, const char *dbName) {
+    int rc = sqlite3_open(dbName, &db);
     if (rc != SQLITE_OK) {
-        std::cerr << "Error opening the database: " << sqlite3_errmsg(db) << std::endl;
-        return rc;
+        throw SQLiteException("Error opening the database: " + std::string(sqlite3_errmsg(db)));
     }
+}
 
-    // Create Table SQL Query
-    const char *createTableSQL = "CREATE TABLE IF NOT EXISTS example_table (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);";
-
-    // Create the table
-    rc = sqlite3_exec(db, createTableSQL, 0, 0, 0);
+/**
+ * @brief Execute SQL Statement
+ *
+ * This function executes a given SQL statement.
+ *
+ * @param[in] db SQLite database connection
+ * @param[in] sqlStatement SQL statement to be executed
+ * @throw SQLiteException on failure
+ */
+void executeSqlStatement(sqlite3 *db, const char *sqlStatement) {
+    int rc = sqlite3_exec(db, sqlStatement, 0, 0, 0);
     if (rc != SQLITE_OK) {
-        std::cerr << "Error creating the table: " << sqlite3_errmsg(db) << std::endl;
         sqlite3_close(db);
-        return rc;
+        throw SQLiteException("Error executing SQL statement: " + std::string(sqlite3_errmsg(db)));
     }
 
-    // Insert Data SQL Query
-    const char *insertDataSQL = "INSERT INTO example_table (name, age) VALUES ('John', 25), ('Alice', 30), ('Bob', 22);";
+    // No need to close the database connection here; it's already done in the functions
+}
 
-    // Insert data into the table
-    rc = sqlite3_exec(db, insertDataSQL, 0, 0, 0);
-    if (rc != SQLITE_OK) {
-        std::cerr << "Error inserting data: " << sqlite3_errmsg(db) << std::endl;
-        sqlite3_close(db);
-        return rc;
-    }
-
-    // Execute SELECT Query and Print Data
-    const char *selectDataSQL = "SELECT * FROM example_table;";
-
-    sqlite3_exec(db, selectDataSQL, [](void *data, int argc, char **argv, char **colName) -> int {
+/**
+ * @brief Execute SELECT Statement and Print Results
+ *
+ * This function executes a SELECT SQL statement and prints the results.
+ *
+ * @param[in] db SQLite database connection
+ * @param[in] selectStatement SELECT SQL statement to be executed
+ * @throw SQLiteException on failure
+ */
+void executeAndPrintSelectStatement(sqlite3 *db, const char *selectStatement) {
+    int rc = sqlite3_exec(db, selectStatement, [](void *data, int argc, char **argv, char **colName) -> int {
         for (int i = 0; i < argc; i++) {
             std::cout << colName[i] << ": " << (argv[i] ? argv[i] : "NULL") << " | ";
         }
@@ -55,8 +85,52 @@ int main() {
         return 0;
     }, 0, 0);
 
-    // Close the database connection
-    sqlite3_close(db);
+    if (rc != SQLITE_OK) {
+        sqlite3_close(db);
+        throw SQLiteException("Error executing SELECT statement: " + std::string(sqlite3_errmsg(db)));
+    }
+
+    // No need to close the database connection here; it's already done in the functions
+}
+
+/**
+ * @brief Main function for the SQLite demonstration program.
+ *
+ * This function demonstrates basic SQLite operations:
+ * - Opening a database
+ * - Creating a table
+ * - Inserting data into the table
+ * - Executing a SELECT query and printing the results
+ *
+ * @return 0 on successful execution, 1 on error
+ */
+int main() {
+    sqlite3 *db;
+
+    try {
+        // Open the database
+        openDatabase(db, "example.db");
+
+        // Create Table SQL Query
+        const char *createTableSQL = "CREATE TABLE IF NOT EXISTS example_table (id INTEGER PRIMARY KEY, name TEXT, age INTEGER);";
+        executeSqlStatement(db, createTableSQL);
+
+        // Insert Data SQL Query
+        const char *insertDataSQL = "INSERT INTO example_table (name, age) VALUES ('John', 25), ('Alice', 30), ('Bob', 22);";
+        executeSqlStatement(db, insertDataSQL);
+
+        // Execute SELECT Query and Print Data
+        const char *selectDataSQL = "SELECT * FROM example_table;";
+        executeAndPrintSelectStatement(db, selectDataSQL);
+
+        // Close the database
+        sqlite3_close(db);
+
+
+    } catch (const SQLiteException &ex) {
+        std::cerr << "SQLite Exception: " << ex.what() << std::endl;
+        return 1;
+    }
 
     return 0;
 }
